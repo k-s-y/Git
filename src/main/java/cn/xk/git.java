d0012dc7dc837da949f0cde61bcb9481f065d380
柯书洋 <2998457809@qq.com>
2022-07-10 19:13:55 +0800

    master1

--- src/main/java/cn/xk/git.java
+++ src/main/java/cn/xk/git.java
@@ -6,5 +6,6 @@ public class git {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("master");
+        System.out.println("master");
     }
 }
