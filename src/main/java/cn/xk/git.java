e3c72b509909224a8ac23f99b853605a45ac02d2
柯书洋 <2998457809@qq.com>
2022-07-10 19:06:24 +0800

    dev提交2

--- src/main/java/cn/xk/git.java
+++ src/main/java/cn/xk/git.java
@@ -5,5 +5,6 @@ public class git {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("devtest");
     }
 }
